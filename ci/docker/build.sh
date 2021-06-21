013c858eab8c23170a6b23ee381ba4eda0c1535c
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2021-06-21 03:09:41 +0200

    ci: switch to new github container registry

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #REPO=registry.gitlab.vision.in.tum.de/rootba/rootba
-REPO=docker.pkg.github.com/nikolausdemmel/rootba_testing
+REPO=ghcr.io/nikolausdemmel/rootba
 
 docker build --pull -f Dockerfile_18.04 -t $REPO/ubuntu-ci-rootba:18.04 .
 docker build --pull -f Dockerfile_20.04 -t $REPO/ubuntu-ci-rootba:20.04 .
